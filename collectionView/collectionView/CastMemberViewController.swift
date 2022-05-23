adb94e3520c2a7a2b35b5a76f26c003c08ff8595
Aigerim Abdurakhmanova <aigerim.abdurakhmanova@administrators-iMac-9.local>
2022-05-23 15:58:57 +0600

    cast info added

---
+++ collectionView/collectionView/CastMemberViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CastMemberViewController.swift
+//  collectionView
+//
+//  Created by Aigerim Abdurakhmanova on 23.05.2022.
+//
+
+import UIKit
+
+class CastMemberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
